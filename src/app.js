fc3476631e3387ebd6b324a790db95c0784a6c22
rb7a <130118715+rb7a@users.noreply.github.com>
2024-09-02 13:20:38 +0800

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 //import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 //import { PageLoader } from "./components/page-loader";
 //import { AuthenticationGuard } from "./components/authentication-guard";
 //import { AdminPage } from "./pages/admin-page";
@@ -23,23 +23,9 @@ export const App = () => {
 //  }
 
   return (
-    <Routes>
+
       <Route path="/" element={<HomePage />} />
-//      <Route
-//        path="/profile"
-//        element={<AuthenticationGuard component={ProfilePage} />}
-//      />
- //     <Route path="/public" element={<PublicPage />} />
-//      <Route
-//        path="/protected"
-//        element={<AuthenticationGuard component={ProtectedPage} />}
-//      />
-//      <Route
-//        path="/admin"
-  //      element={<AuthenticationGuard component={AdminPage} />}
-//      />
-//      <Route path="/callback" element={<CallbackPage />} />
-//      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+
+
   );
 };
