49f950987e2d95965ed07f1609cecba05db82841
rb7a <130118715+rb7a@users.noreply.github.com>
2024-09-02 12:57:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "./app";
-import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
+//import { Auth0ProviderWithNavigate } from "./auth0-provider-with-navigate";
 import "./styles/styles.css";
 
 const container = document.getElementById("root");
@@ -11,9 +11,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Auth0ProviderWithNavigate>
+//      <Auth0ProviderWithNavigate>
         <App />
-      </Auth0ProviderWithNavigate>
+//      </Auth0ProviderWithNavigate>
     </BrowserRouter>
   </React.StrictMode>
 );
